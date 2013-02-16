d28414073dbcd87150ec1f23f84a2dd0a343ecc7
gerow <gerow.mike@gmail.com>
2013-02-16 02:57:39 -0800

    Rearranged project to make building debs easier

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-go install github.com/gerow/boop
+go install github.com/gerow/boop/boop
 cp -r $GOPATH/src/github.com/gerow/boop/etc/boop /etc
 cp $GOPATH/src/github.com/gerow/boop/etc/init/boop.conf /etc/init
